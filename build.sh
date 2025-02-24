75d137c2828b6c3e926aca6121d39a1eb9b78ad9
Ryan King <ryan.king157@gmail.com>
2025-02-24 15:54:48 -0800

    debug

--- build.sh
+++ build.sh
@@ -8,11 +8,17 @@ cmd_dirs=$(find cmd -maxdepth 1 -type d -not -path "cmd")
 
 # Build each command in its respective directory
 for dir in $cmd_dirs; do
-  command_name=$(basename "$dir")  # Extract command name
+    command_name=$(basename "$dir")  # Extract command name
 
-  echo "Building command: $command_name"
-  go build -gcflags="all=-N -l" -o "./$command_name" "./$dir"
-  echo "Command built successfully: $command_name"
+    echo "Building command: $command_name"
+
+    if [[ "$*" == *"--debug"* ]]; then
+      go build -gcflags="all=-N -l" -o "./$command_name" "./$dir"
+      echo "Debug build of command built successfully: $command_name"
+    else
+      go build -o "./$command_name" "./$dir"
+      echo "Command built successfully: $command_name"
+    fi
 
 done
 
